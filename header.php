127136e8376d1a953ff94ce3892cfeed06678141
Bruce Tang <bruce@brucetangdesign.com>
2018-09-25 11:22:21 -0400

    Fixed mailto link

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
     <ul class="hr-list">
       <li><a class="products-link" href="<?php if($page=="about-page"){ echo "index.php"; }?>#about-products">Products</a></li>
       <li><a href="about.php" <?php if($page=="about-page"){ echo "class='selected'"; }?>>About</a></li>
-      <li><a href="mail:sweetleaftea@casupport.com">Contact</a></li>
+      <li><a href="mailto:sweetleaftea@casupport.com">Contact</a></li>
     </ul>
   </nav>
 </header>
